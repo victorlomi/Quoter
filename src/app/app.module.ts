076fdbdafde0483c9d042464678c79f466d25285
plutos28 <gamevictor28@gmail.com>
2020-11-09 13:11:05 +0300

    add add-quote component with button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeroComponent } from './hero/hero.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { AddQuoteComponent } from './add-quote/add-quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HeroComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    AddQuoteComponent
   ],
   imports: [
     BrowserModule,
