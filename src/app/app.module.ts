61d5c472a08db8945a79d35010a2327e304be5a5
plutos28 <gamevictor28@gmail.com>
2020-11-09 12:30:26 +0300

    add categories section component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeroComponent } from './hero/hero.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HeroComponent
+    HeroComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
