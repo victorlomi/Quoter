b0a6c25d08152eb3ebaf0d2e1b5aadd1ca009335
plutos28 <gamevictor28@gmail.com>
2020-11-10 06:19:31 +0300

    confirm user submission and go to homepage

--- src/app/add-quote-form/add-quote-form.component.ts
+++ src/app/add-quote-form/add-quote-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
-import { CategoriesService } from '../categories.service'
+import { CategoriesService } from '../categories.service';
 
 @Component({
   selector: 'app-add-quote-form',
@@ -13,6 +14,7 @@ export class AddQuoteFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private categoriesService: CategoriesService,
+    private router: Router
   ) {
     this.addQuoteForm = this.formBuilder.group({
       text: '',
@@ -26,9 +28,15 @@ export class AddQuoteFormComponent implements OnInit {
   }
 
   onSubmit(userData) {
-    console.log("Form is submitted!", userData);
-    this.categoriesService.addQuote(userData);
+    let category = this.categoriesService.getCategories()[userData.category];
 
+    console.log("Form is submitted!", userData);
+    alert(`Your quote was added to the ${category.title} category`);
+
+    this.categoriesService.addQuote(userData);
     this.addQuoteForm.reset();
+
+    // leave to homepage
+    this.router.navigateByUrl("");
   }
 }
