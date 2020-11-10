bc4bffdd00897998c88e99a4bfde8bb4ed69166c
plutos28 <gamevictor28@gmail.com>
2020-11-10 05:22:09 +0300

    show quotes from categories service

--- src/app/category-box/category-box.component.ts
+++ src/app/category-box/category-box.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Quote } from "../quote";
+
 @Component({
   selector: 'app-category-box',
   templateUrl: './category-box.component.html',
   styleUrls: ['./category-box.component.css']
 })
 export class CategoryBoxComponent implements OnInit {
-  @Input() text: string;
-  @Input() author: string;
+  @Input() quote: Quote;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/show-category/show-category.component.ts
+++ src/app/show-category/show-category.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { CategoriesService } from '../categories.service';
+
 @Component({
   selector: 'app-show-category',
   templateUrl: './show-category.component.html',
   styleUrls: ['./show-category.component.css']
 })
 export class ShowCategoryComponent implements OnInit {
-  text1 = "hello world";
-  author1 = "me me me"
-  constructor() { }
+  category;
+  constructor(
+    private categoriesService: CategoriesService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.category = this.categoriesService.getCategories()[params.get('categoryId')];
+    })
   }
 
 }
