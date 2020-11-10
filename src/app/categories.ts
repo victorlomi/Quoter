112a4f5fe8c9dc2583da73dc29bf30502aad26b2
plutos28 <gamevictor28@gmail.com>
2020-11-10 05:36:27 +0300

    make quotes reflect their categories

--- src/app/categories.ts
+++ src/app/categories.ts
@@ -8,13 +8,15 @@ export let firstCategories: Category[] = [
     {text: "Dreams are not what you see in your sleep, dreams are things which do not let you sleep.", author: "Cristiano Ronaldo", username: "hardwork28"},
   ]},
   {title: "TV Shows", "image": "tv.jpg", quotes: [
-    {text: "If you quit once it becomes a habit. Never quit!", author: "Micheal Jordan", username: "jason12"},
-    {text: "I do it because I love it and that's all I care about", author: "Lionel Messi", username: "ilovefootball"},
-    {text: "Dreams are not what you see in your sleep, dreams are things which do not let you sleep.", author: "Cristiano Ronaldo", username: "hardwork28"},
+    {text: "We want to be sedated. Because it's painful not to pretend, because we're cowards", author: "Elliot Alderson", username: "hackerboy123"},
+    {text: "The only time a man can be brave is when he's afraid", author: "Ned Stark", username: "mythrone"},
+    {text: "I'm not in the meth business. I'm in the empire business", author: "Walter White", username: "businessman"},
+    {text: "Mozart, Beethoven and Chopin never died. They simply became music", author: "Ford", username: "harshworld27"},
+    {text: "You come at the King, you best not miss", author: "Omar Little", username: "wires@28"},
   ]},
   {title: "Science", "image": "science.jpg", quotes: [
-    {text: "If you quit once it becomes a habit. Never quit!", author: "Micheal Jordan", username: "jason12"},
-    {text: "I do it because I love it and that's all I care about", author: "Lionel Messi", username: "ilovefootball"},
-    {text: "Dreams are not what you see in your sleep, dreams are things which do not let you sleep.", author: "Cristiano Ronaldo", username: "hardwork28"},
+    {text: "Science is simple the word we use to describe a method of organizing our curiosity", author: "Tom Minchin", username: "iseeworld"},
+    {text: "Science is a way of thinking much more than it is a body of knowledge", author: "Carl Sagan", username: "third@eye"},
+    {text: "We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology", author: "Carl Sagan", username: "hardwork28"},
   ]},
 ];
